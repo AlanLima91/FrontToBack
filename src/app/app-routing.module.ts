704d0602fea9546246c05d877b8805a1ef3e3ff9
AlanLima91 <m.limaalan@hotmail.fr>
2018-12-05 12:14:49 +0100

    Bootstrap Add; Nav Add; Route Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { BasketComponent } from './basket/basket.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProfilComponent } from './profil/profil.component';
 
 const routes: Routes = [
-  
+  { path: 'accueil', component: MenuComponent},
+  { path: 'panier', component: BasketComponent},
+  { path: 'auth', component: AuthComponent},
+  { path: 'profil', component: ProfilComponent},
 ];
 
 @NgModule({
