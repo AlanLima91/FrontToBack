899f72ce95d16ec4d6df84fba4fe600f18920098
AlanLima91 <m.limaalan@hotmail.fr>
2018-12-05 12:01:39 +0100

    Add component auth profil basket menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BasketComponent } from './basket/basket.component';
+import { ProfilComponent } from './profil/profil.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BasketComponent,
+    ProfilComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
