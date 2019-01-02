b4bbc962d999c108979d53a0a9cf5930e4b87848
AlanLima91 <m.limaalan@hotmail.fr>
2019-01-02 10:49:29 +0100

    clean the error with the user object in the order object

--- src/app/administration/administration.component.ts
+++ src/app/administration/administration.component.ts
@@ -11,22 +11,21 @@ import { User } from '../user';
 export class AdministrationComponent implements OnInit
 {
   list:Order[];
-  order:Order;
+
   constructor(private allService:AllService) { }
 
   ngOnInit()
   {
     this.getOrders();
-    this.allService.getUserByKey("-LT7EJHxqq4Zycn7lX36").subscribe(data => {
-      let user = Object.values(data);
-      // this.order = new Order(null, user, 5, null,);
-      console.log(user);
-    });
+    // this.allService.getUserByKey("-LT7EJHxqq4Zycn7lX36").subscribe(data => {
+    //   let user = Object.values(data);
+    // });
     // this.allService.addOrder(this.order).subscribe();
   }
 
   getOrders()
   {
+    let test = new Order(void, void, 13, void);
     this.allService.getOrders().subscribe(data =>
       {
         this.list = Object.values(data);
