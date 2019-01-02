750c4e3bf0a2ec98866f0f85469a2b1c141efa88
Dlyf <domilung@gmail.com>
2019-01-02 12:12:59 +0100

    test null

--- src/app/administration/administration.component.ts
+++ src/app/administration/administration.component.ts
@@ -25,7 +25,6 @@ export class AdministrationComponent implements OnInit
 
   getOrders()
   {
-    let test = new Order(void, void, 13, void);
     this.allService.getOrders().subscribe(data =>
       {
         this.list = Object.values(data);
