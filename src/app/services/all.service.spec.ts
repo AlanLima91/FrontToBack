075a02766dbfcf6424a583fbd944d62b1c76932f
Dlyf <domilung@gmail.com>
2019-02-01 23:24:10 +0100

    delete allservice ?

--- src/app/services/all.service.spec.ts
+++ src/app/services/all.service.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import {throwError as observableThrowError,  Observable } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
-import { AllService } from './all.service';
+// import { TestBed } from '@angular/core/testing';
+// import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+// import {throwError as observableThrowError,  Observable } from 'rxjs';
+// import { tap, catchError } from 'rxjs/operators';
+// import { AllService } from './all.service';
 
-describe('AllService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+// describe('AllService', () => {
+//   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: AllService = TestBed.get(AllService);
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     const service: AllService = TestBed.get(AllService);
+//     expect(service).toBeTruthy();
+//   });
+// });
